4ca926ae2d31a068a81ac854f35af422c595fc3b
Ilia Chuvikin <iliaivanovskiy@mail.ru>
2024-07-19 18:08:32 +0300

    Database connected

--- src/main/java/com/newtelegrambot/devyNewBot/models/User.java
+++ src/main/java/com/newtelegrambot/devyNewBot/models/User.java
@@ -2,12 +2,10 @@ package com.newtelegrambot.devyNewBot.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 import org.glassfish.grizzly.http.util.TimeStamp;
 
 @Data
-@Table(name = "")
 @Entity
 public class User {
 
